b6e114bdb3040b959806683df1024b215fc8bce6
João Fernando Rangel <joaofrguimaraes1@gmail.com>
2024-12-15 10:19:28 -0300

    leitura de potenciomentro e gerenciamento de tráfico de pacotes

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,51 @@
 #include <Arduino.h>
-#include <SPI.h>
 #include <LoRa.h>
+#include <SPI.h>
 
+#define PotPin A4
+#define noiseThrs 20
 void sendString(String send);
+bool handleAnalogRead(uint8_t pin, int &pot, int noise);
 
 unsigned long t[5];
 uint16_t counter;
+int potRead;
 
-void setup()
-{
-  Serial.begin(9600);
-  while (!Serial)
-    ;
+void setup() {
+    Serial.begin(9600);
+    Serial.println("Init Setup");
+    Serial.println("LoRa Sender");
+    pinMode(PotPin, INPUT);
+    if (!LoRa.begin(433E6)) {
+        Serial.println("Starting LoRa failed!");
+        while (1);
+    }
+    // LoRa.setSyncWord(0xF3);
+    potRead = analogRead(PotPin);
+    Serial.println("Finish Setup");
+}
 
-  Serial.println("LoRa Sender");
-
-  if (!LoRa.begin(915E6))
-  {
-    Serial.println("Starting LoRa failed!");
-    while (1)
-      ;
+String sender = "Sending packet: ";
+void loop() {
+  if(handleAnalogRead(PotPin, potRead, noiseThrs)){
+    Serial.print("Sending packet: ");
+    Serial.println(potRead);
+    sendString(String(potRead));
   }
 }
 
-void loop()
-{
-  String sender = "Sending packet: ";
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
-
-
-  sendString(sender + String(counter));
-
-  counter++;
-
-  delay(5000);
+bool handleAnalogRead(uint8_t pin, int &pot, int noise) {
+    int read1 = analogRead(pin);
+    if (abs((abs(read1) - abs(pot))) > noise) {
+        pot = read1;
+        return true;
+    } else {
+        return false;
+    }
 }
 
-void sendString(String send)
-{
-  LoRa.beginPacket();
-  LoRa.print(send.c_str());
-  LoRa.endPacket();
+void sendString(String send) {
+    LoRa.beginPacket();
+    LoRa.print(send.c_str());
+    LoRa.endPacket();
 }
