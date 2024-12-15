611965e03b7efa96924c5d9068d0ebb94257bc6f
João Fernando Rangel <joaofrguimaraes1@gmail.com>
2024-12-15 14:21:03 -0300

    envia valores aleatotorios para o receptor, organiza a mensagem para ser dividida em JSON depois

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include <SPI.h>
 
 #define PotPin A4
-#define noiseThrs 20
+#define noiseThrs 25
+#define idMAX 5
 void sendString(String send);
 bool handleAnalogRead(uint8_t pin, int &pot, int noise);
 
@@ -20,18 +21,35 @@ void setup() {
         Serial.println("Starting LoRa failed!");
         while (1);
     }
-    // LoRa.setSyncWord(0xF3);
+    LoRa.setSyncWord(0xF3);
     potRead = analogRead(PotPin);
     Serial.println("Finish Setup");
 }
 
 String sender = "Sending packet: ";
 void loop() {
-  if(handleAnalogRead(PotPin, potRead, noiseThrs)){
-    Serial.print("Sending packet: ");
-    Serial.println(potRead);
-    sendString(String(potRead));
-  }
+    // if (handleAnalogRead(PotPin, potRead, noiseThrs)) {
+    //     Serial.print("Sending packet: ");
+    //     Serial.println(potRead);
+    //     counter++;
+    //     sendString(String(potRead));
+    // }
+    if (millis() - t[0] > 5000) {
+        float value[idMAX];
+        String toGo;
+        int i = 0;
+        while (i < idMAX) {
+            value[i] = (static_cast<float>(random(0, 100)) + 0.01 * static_cast<float>(random(0, 50)));
+            toGo += "id" + String(i) + "_" + value[i] + ";";
+            i++;
+        }
+        toGo += "id" + String(idMAX) + "_" + counter + ";";
+        Serial.print("Sending packet: ");
+        Serial.println(toGo);
+        sendString(toGo);
+        counter++;
+        t[0] = millis();
+    }
 }
 
 bool handleAnalogRead(uint8_t pin, int &pot, int noise) {
